76c77de53727d7f96622e3e0537d174ae3af624f
Robin Pahlman <robin.pahlman@gmail.com>
2018-04-20 14:42:38 -0400

    toggle div visibility with ternary expression

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
       { name: 'Heli', age: 33 },
       { name: 'Robin', age: 38 },
       { name: 'Charlie', age: 7 }
-    ]
+    ],
+    showPersons: false
   }
 
   switchNameHandler = (newName) => {
@@ -20,25 +21,59 @@ class App extends Component {
       ]
     })
   }
+
+  nameChangedHandler = (event) => {
+    this.setState({
+      people: [
+        { name: event.target.value, age: 27 },
+        { name: 'Robin', age: 38 },
+        { name: 'Charlie', age: 8 }
+      ]
+    });
+  }
+
+  togglePersonHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({
+      showPersons: !doesShow
+    });
+  }
   render() {
+
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer',
+    }
     return (
       <div className="App">
         <h1>I'm a React App!</h1>
-        <button onClick={this.switchNameHandler.bind(this, 'Matheo')}>Switch Name</button>
-        <Person 
-          name={this.state.people[0].name} 
-          age={this.state.people[0].age} 
+        <button 
+          style={style}
+          onClick={this.togglePersonHandler}>
+          Toggle Persons
+        </button>
+        { this.state.showPersons ? 
+          <div>
+          <Person
+            name={this.state.people[0].name}
+            age={this.state.people[0].age}
+            changed={this.nameChangedHandler}
           />
-        <Person 
-          name={this.state.people[1].name} 
-          age={this.state.people[1].age}
-          click={this.switchNameHandler.bind(this, 'Max!!!')}>
+          <Person
+            name={this.state.people[1].name}
+            age={this.state.people[1].age}
+            click={this.switchNameHandler.bind(this, 'Max!!!')}>
             My hobbies: Reading
           </Person>
-        <Person 
-          name={this.state.people[2].name} 
-          age={this.state.people[2].age}
+          <Person
+            name={this.state.people[2].name}
+            age={this.state.people[2].age}
           />
+        </div> : null
+        }
       </div>
     );
   }
