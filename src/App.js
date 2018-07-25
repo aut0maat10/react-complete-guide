b7a2da3d5c037e591436d6f73962552719f3b55d
Robin Pahlman <robin.pahlman@gmail.com>
2018-07-25 11:41:12 -0400

    set up css modules

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from'./App.css';
 import Person from './Person/Person'; 
 // import Radium, { StyleRoot } from 'radium';
 
@@ -49,7 +49,7 @@ class App extends Component {
       color: 'white',
       padding: '8px',
       cursor: 'pointer',
-      // ':hover': {
+      // ':hover': { // RADIUM
       //   backgroundColor: 'lightgreen',
       //   color: 'black',
       // }
@@ -77,18 +77,18 @@ class App extends Component {
       // }
     }
 
-    const classes = [];
+    const assignedClasses = [];
     if (this.state.people.length <= 2) {
-      classes.push('red'); // classes = ['red']
+      assignedClasses.push( classes.red ); // classes = ['red']
     }
     if (this.state.people.length <= 1) {
-      classes.push('bold'); // classes = ['red, 'bold']
+      assignedClasses.push( classes.bold ); // classes = ['red, 'bold']
     }
 
     return (
-      <div className="App">
+      <div className={classes.App}>
         <h1>I'm a React App!</h1>
-        <p className={classes.join(' ')}>This is really working!</p>
+        <p className={assignedClasses.join(' ')}>This is really working!</p>
         <button 
           style={style}
           onClick={this.togglePersonHandler}>
